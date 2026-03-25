e943ac04e93d3a5df3c1f90096148de23a250ac2
CaduCarfi <157553755+CaduCarfi@users.noreply.github.com>
2026-03-25 17:02:15 -0300

    Cria missoes model e inicia relacionamento entre tabelas

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -0,0 +1,17 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_missoes")
+public class MissaoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String dificuldade;
+
+    private NinjaModel ninja;
+}
