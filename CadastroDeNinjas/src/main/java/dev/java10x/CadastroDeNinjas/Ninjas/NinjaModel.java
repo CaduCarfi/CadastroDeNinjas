06e909be3c2b5da294919ba5adb31dd2770e4f05
CaduCarfi <157553755+CaduCarfi@users.noreply.github.com>
2026-08-07 11:03:13 -0300

    fix: corrige estrutura do repositorio, remove submodule indevido

---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -0,0 +1,41 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Entity trasnforma uma classe em uma entidade do BD
+// JPA = Java Persistence API
+@Entity
+@Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "idade")
+    private int idade;
+
+    // @ManyToOne - um ninja tem uma unica missao
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") // Foreing Key ou chave estrangeira
+    private MissoesModel missoes;
+
+
+}
